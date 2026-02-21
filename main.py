1cead1df2fae49b1c496a88ecd85cf3e2dca0fe3
Artheek18 <artheek2321.Shan@gmail.com>
2026-02-21 02:22:15 -0500

    able to send an email with articls from the news API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from send_email import send_email
 api_key = "1abb172f46224091956d2b49ef0da16a"
 url = ('https://newsapi.org/v2/everything?q=apple&from'
        '=2026-02-20&to=2026-02-20&sortBy=popularity&apiKey=') + api_key
@@ -14,9 +14,15 @@ content = response.json()
 #print(type(content))
 print(content)
 
+article = content["articles"][0]
+
+title = article.get("title", "No title")
+description = article.get("description", "")
+
+body = f"{title}\n{description}\n\n"
+send_email(body)
 for item in content['articles']:
-    print(item['title'], item['author'])
-    print(item['description'])
+    body += item['title'] + '\n' + item['description'] + 2*'\n'
 
 '''response = requests.get(url)
 text = response.text
--- send_email.py
+++ send_email.py
@@ -1,14 +1,17 @@
 import smtplib, ssl
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "app8flask@gmail.com"
-    password = "YOUR_GMAIL_PASSWORD"
+    username = os.getenv("EMAIL_ADDRESS")
+    password = os.getenv("EMAIL_PASSWORD")
 
-    receiver = "app8flask@gmail.com"
+    receiver = "artheek.Shanmugalingam@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
